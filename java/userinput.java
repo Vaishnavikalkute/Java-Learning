18504c9ebc13c3535b03866f8a3d486ba3bf85bd
mtab <mtab@vitbangalore.in>
2024-09-04 18:41:56 +0530

    try chnage

--- java/userinput.java
+++ java/userinput.java
@@ -8,7 +8,6 @@ public class userinput {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
 
-
         System.out.println("Enter the name");
         String name=scanner.nextLine();
         System.out.println("Enter the value");
